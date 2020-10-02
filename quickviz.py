4deb8330daeef46625bd476620dc0012e120a727
Michael Paquette <jotha885@gmail.com>
2020-10-02 17:09:01 +0200

    wip

--- quickviz.py
+++ quickviz.py
@@ -184,7 +184,7 @@ def main():
         data_hist = data_hist[data_hist <= hist_max]
 
         print('Histogram of masked volume with {} bins from {:.4f} to {:.4f}'.format(nbins, hist_min, hist_max))
-	print('mean = {:.4f}   std = {:.4f}'.format(data_hist.mean(), data_hist.std()))
+        print('mean = {:.4f}   std = {:.4f}'.format(data_hist.mean(), data_hist.std()))
         pl.figure()
         pl.hist(data_hist, bins = nbins)
 
